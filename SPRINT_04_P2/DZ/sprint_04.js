fc12627ee8f2b0db4fd4fd6acca9bfd1a7fa36a1
Yartes83 <yartes83@gmail.com>
2025-06-12 15:55:12 +0300

    first commit

---
+++ SPRINT_04_P2/DZ/sprint_04.js
@@ -0,0 +1,238 @@
+// iTGid.info - курс JS24/2
+
+// В задачах, где говорится пробел - применяем символ пробела - нажатие на клавишу space.
+// Там где говорится дефис - подразумевается черта на клавиатуре "минус", между символами 0 и =
+
+// Task 01
+// Создайте функцию. Функция с помощью вложенных циклов создает строку:
+// 4=4 3 2 1 0 3=4 3 2 1 0 2=4 3 2 1 0 1=4 3 2 1 0 0=4 3 2 1 0
+// и выводит в .out-1.
+// Напоминаю. Если можно избежать, то не делайте вывод на страницу внутри цикла.
+
+const t01 = () => {
+   
+};
+
+document.querySelector('.b-1').addEventListener('click', t01);
+
+// Task 02
+// Создайте функцию. Функция с помощью вложенных циклов создает строку:
+// 0****<br>1****<br>2****<br>3****<br>4****<br>
+// и выводит в .out-2 с помощью innerHTML
+
+const t02 = () => {
+   
+};
+
+document.querySelector('.b-2').addEventListener('click', t02);
+
+
+// Task 03
+// Функция должна выводить прямоугольник на страницу состоящий из символов звездочка. В горизонтали 6 звездочек, количество линий - число, которое пользователь ввел в input.i-3.
+// Перенос строки делайте с помощью тега br.
+
+const t03 = () => {
+    
+};
+
+document.querySelector('.b-3').addEventListener('click', t03);
+
+
+// Task 04
+// Функция должна выводить прямоугольник на страницу состоящий из чисел. В горизонтали 6 чисел от 5 до нуля, количество линий - число, которое пользователь ввел в input.i-4.
+// Перенос строки делайте с помощью тега br.
+
+const t04 = () => {
+   
+};
+
+document.querySelector('.b-4').addEventListener('click', t04);
+
+
+// Task 05
+// Функция должна выводить прямоугольник на страницу состоящий из чисел. В горизонтали числа от введенного в input.i-51 до нуля, количество линий - число, которое пользователь ввел в input.i-52.
+// Перенос строки делайте с помощью тега br.
+
+const t05 = () => {
+   
+};
+
+document.querySelector('.b-5').addEventListener('click', t05);
+
+
+// Task 06
+// Функция должна с помощью циклов рисовать такую фигуру:
+// и выводить в .out-6
+
+const t06 = () => {
+   
+};
+
+document.querySelector('.b-6').addEventListener('click',  t06);
+
+
+// Task 07
+// Функция должна с помощью циклов рисовать такую фигуру:
+// и выводить в .out-7.
+
+
+const t07 = () => {
+    
+};
+
+document.querySelector('.b-7').addEventListener('click', t07);
+
+
+// Task 08
+// Функция должна рисовать такую фигуру
+//10x01x%
+//10x01x
+//10x01x
+// и выводить в .out-08
+
+const t08 = () => {
+    
+};
+
+document.querySelector('.b-8').addEventListener('click', t08);
+
+
+// Task 09
+// Функция должна рисовать такую фигуру
+// и выводить в .out-09
+
+const t09 = () => {
+   
+};
+
+document.querySelector('.b-9').addEventListener('click', t09);
+
+
+// Task 10
+// Функция должна вывести в .out-10 такую строку:
+// 5 0 4 1 3 2 2 3 1 4 0 5
+
+const t10 = () => {
+   
+};
+
+document.querySelector('.b-10').addEventListener('click',  t10);
+
+
+// Task 11
+// Функция должна рисовать такую фигуру
+// и выводить в .out-11
+
+const t11 = () => {
+   
+};
+
+document.querySelector('.b-11').addEventListener('click',  t11);
+
+
+
+// Task 12
+// Функция должна рисовать такую фигуру
+// и выводить в .out-12
+
+const t12 = () => {
+    
+};
+
+document.querySelector('.b-12').addEventListener('click', t12);
+
+
+// Task 13
+// Функция должна рисовать такую фигуру
+// и выводить в .out-13
+
+
+const t13 = () => {
+   
+};
+
+document.querySelector('.b-13').addEventListener('click',t13);
+
+
+// Task 14
+// Функция должна рисовать такую фигуру
+// и выводить в .out-14
+
+
+const t14 = () => {
+   
+};
+
+document.querySelector('.b-14').addEventListener('click', t14);
+
+// Task 15
+// Функция должна рисовать такую фигуру
+// и выводить в .out-15
+
+
+const t15 = () => {
+   
+};
+
+document.querySelector('.b-15').addEventListener('click', t15);
+
+
+// Task 16
+// Функция должна рисовать такую фигуру
+// и выводить в .out-16
+
+
+const t16 = () => {
+   
+};
+
+document.querySelector('.b-16').addEventListener('click', t16);
+
+
+// Task 17
+// Функция должна рисовать такую фигуру
+// и выводить в .out-17
+
+
+const t17 = () => {
+  
+};
+
+document.querySelector('.b-17').addEventListener('click', t17);
+
+
+// Task 18
+// Функция должна рисовать такую фигуру
+// и выводить в .out-18
+
+
+const t18 = () => {
+ 
+};
+
+document.querySelector('.b-18').addEventListener('click', t18);
+
+
+
+// Task 19
+// Функция должна рисовать такую фигуру
+// и выводить в .out-19
+
+
+const t19 = () => {
+  
+};
+
+document.querySelector('.b-19').addEventListener('click', t19);
+
+
+// Task 20
+// Функция должна рисовать такую фигуру
+// и выводить в .out-20
+
+
+const t20 = () => {
+   
+};
+
+document.querySelector('.b-20').addEventListener('click', t20);
